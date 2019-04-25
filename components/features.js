783eb1e90976979d6450215a34bc2ad17ae67386
HaBuDeSu <ruf.kenichi@gmail.com>
2019-04-24 21:50:16 -0500

    adding features and creating FeatureTab and FeatureDetail components for accordion

--- components/features.js
+++ components/features.js
@@ -1,7 +1,12 @@
-new FeatureSummary {
-
+new FeatureTab(tabElement) {
+  this.tabElement = tabElement;
+  this.tabName = tabElement.querySelector(".feature-name");
 }
 
 new FeatureDetail {
 
 }
+
+let tabs = document.querySelectorAll(".feature-tab");
+tabs.forEach((tab, i) => tab.setAttribute("data-tab", `${i}`));
+tabs.forEach(tab => new FeatureTab(tab));
