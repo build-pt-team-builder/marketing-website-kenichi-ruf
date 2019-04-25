8963b75da682b4416f65413b83d145448a9207c5
HaBuDeSu <ruf.kenichi@gmail.com>
2019-04-24 22:45:08 -0500

    accordion now toggles

--- components/features.js
+++ components/features.js
@@ -3,11 +3,16 @@ class Feature {
     this.featureElement = featureElement;
     this.featureTab = this.featureElement.querySelector(".feature-tab");
     this.featureDetail = this.featureElement.querySelector(".feature-details");
+    this.featureDetail.setAttribute("style", "display: none");
     this.featureTab.addEventListener('click', this.expand.bind(this));
   };
 
   expand() {
+    if (this.featureDetail.style.display === "none") {
     this.featureDetail.setAttribute("style", "display: block");
+    } else {
+      this.featureDetail.setAttribute("style", "display: none");
+    }
   }
 }
 
