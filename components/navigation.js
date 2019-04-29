da0de19d3ff8331884c102c0b4ae771a01cce7c0
HaBuDeSu <ruf.kenichi@gmail.com>
2019-04-28 22:35:22 -0500

    got mobile nav links to show up

--- components/navigation.js
+++ components/navigation.js
@@ -1,5 +1,6 @@
 let navigation = document.querySelector("nav");
 let navlinks = document.querySelectorAll("nav a");
 navigation.addEventListener('click', function () {
-  navlinks.forEach(link => link.setAttribute("style", "display: block"))
+  navlinks.forEach(link => link.classList.add("mobile-nav-link"));
+  navlinks.forEach((link, i) => link.setAttribute("style", `top: ${50+50*i}px`));
 });
