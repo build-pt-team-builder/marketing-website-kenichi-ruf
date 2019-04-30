ef91ee677b6f3f95a7549339b80e8f9124fa85a7
HaBuDeSu <ruf.kenichi@gmail.com>
2019-04-29 21:37:49 -0500

    got mobile navigation to transform on click

--- components/navigation.js
+++ components/navigation.js
@@ -4,3 +4,9 @@ navigation.addEventListener('click', function () {
   navlinks.forEach(link => link.classList.add("mobile-nav-link"));
   navlinks.forEach((link, i) => link.setAttribute("style", `top: ${50+50*i}px`));
 });
+
+let mobileNav = document.querySelector(".mobile-nav");
+let bars = document.querySelectorAll(".mobile-nav div");
+mobileNav.addEventListener('click', function () {
+  bars.forEach(bar => bar.classList.toggle("change"));
+});
