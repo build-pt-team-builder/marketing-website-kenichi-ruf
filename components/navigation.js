aee2d0ee96d90cddc503184e5d534d40ba113158
HaBuDeSu <ruf.kenichi@gmail.com>
2019-04-29 22:32:27 -0500

    got mobile nav working correctly. just need to align it

--- components/navigation.js
+++ components/navigation.js
@@ -1,12 +1,11 @@
-let navigation = document.querySelector("nav");
-let navlinks = document.querySelectorAll("nav a");
-navigation.addEventListener('click', function () {
-  navlinks.forEach(link => link.classList.add("mobile-nav-link"));
-  navlinks.forEach((link, i) => link.setAttribute("style", `top: ${50+50*i}px`));
-});
-
+let nav = document.querySelector("nav");
 let mobileNav = document.querySelector(".mobile-nav");
 let bars = document.querySelectorAll(".mobile-nav div");
 mobileNav.addEventListener('click', function () {
   bars.forEach(bar => bar.classList.toggle("change"));
+  if (nav.style.display == "flex") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "flex";
+  }
 });
